78609f9aafd582f43cc769b8e1f9df6de54ac3f1
Ru8icks <rjhansen@stud.ntnu.no>
2017-10-31 16:12:58 +0100

    set up mongodb, cant have identical usernames and log in works, next step make active users visible

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var app=angular.module('myApp',[]);
 app.controller('mainController',['$scope',function($scope){
     var messages = document.getElementById("messages");
+    var modalContent = document.getElementById('modalContent');
     var modal = document.getElementById('myModal');
     var main = document.getElementById("main");
     var username = document.getElementById("username");
@@ -13,12 +14,17 @@ app.controller('mainController',['$scope',function($scope){
 
     };
     $scope.logIn = function(){
+        socket.emit('check user', username.value);
+    };
+    socket.on('new nick',function(user){
+       modalContent.appendChild(document.createElement("p").appendChild(document.createTextNode("the username "+user +" is taken")));
+        console.log("sofarsogod")
+    });
+    socket.on('logIn',function(user){
         main.style.display = "block";
         modal.style.display = "none";
         localStorage.setItem("username", username.value)
-
-
-    }
+    });
     socket.on('chat message', function(msg){
         var li=document.createElement("li");
         li.appendChild(document.createTextNode(msg));
--- server.js
+++ server.js
@@ -2,17 +2,45 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongojs = require('mongojs');
+var db = mongojs('chat', ['chat']);
 
 app.use(express.static(__dirname));
 
 
 io.on('connection', function(socket){
     console.log("a user connected");
-    socket.on('chat message', function(msg,username){
-        io.emit('chat message', msg, username);
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
     });
+
     socket.on('disconnect', function(){
         console.log('a user disconnected');
+        db.chat.remove({name:user});
+    });
+
+    socket.on('check user', function(user){
+    db.chat.count({name :user}, function (err, docs) {
+        if (err) {
+            console.warn(err.message);
+        }
+        else {
+            console.log(docs);
+            if (docs!==0){
+                socket.emit('new nick', user);
+            } else {
+                db.chat.insert({name: user}, function (err, o) {
+                    if (err) {
+                        console.warn(err.message);
+                    }
+                    else {
+                        console.log("user addded to db " + user);
+                    }
+                });
+                socket.emit('logIn', user);
+                }
+            }
+        });
     });
 
 });
