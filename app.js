e4938ae27e1abecb8646cc41fef759a2ed23921b
Ru8icks <rjhansen@stud.ntnu.no>
2017-11-01 13:55:45 +0100

    ACTIVE USERS is visible and responsive when people enter and leave, next step set up indevidual chat yeheya

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 var app=angular.module('myApp',[]);
-app.controller('mainController',['$scope',function($scope){
+app.controller('mainController',['$scope', '$http',function($scope, $http){
     var messages = document.getElementById("messages");
     var modalContent = document.getElementById('modalContent');
     var modal = document.getElementById('myModal');
@@ -7,9 +7,28 @@ app.controller('mainController',['$scope',function($scope){
     var username = document.getElementById("username");
 
     var socket = io.connect();
+
+
+    if (localStorage.getItem("username") !== null)  {
+        $scope.userNick = localStorage.getItem("username")
+
+    }
+
+    var refreshUsers = function(){
+        console.log("whygetnowork")
+        $http.get('/chat').then(function (response) {
+            console.log("got requested dadta");
+            console.log(response.data);
+            $scope.activeUsers = response.data;
+
+
+        });
+    };
+
     $scope.send = function(){
         var msgString = localStorage.getItem("username") + " :"+$scope.message ;
         socket.emit('chat message',msgString );
+        //get request, use socket.id to get current username. fuck localstoragebug/feature.
         $scope.message="";
 
     };
@@ -17,17 +36,21 @@ app.controller('mainController',['$scope',function($scope){
         socket.emit('check user', username.value);
     };
     socket.on('new nick',function(user){
-       modalContent.appendChild(document.createElement("p").appendChild(document.createTextNode("the username "+user +" is taken")));
+        modalContent.appendChild(document.createElement("p").appendChild(document.createTextNode("the username "+user +" is taken")));
         console.log("sofarsogod")
     });
     socket.on('logIn',function(user){
         main.style.display = "block";
         modal.style.display = "none";
-        localStorage.setItem("username", username.value)
+        localStorage.setItem("username", user)
+        refreshUsers();
     });
     socket.on('chat message', function(msg){
         var li=document.createElement("li");
         li.appendChild(document.createTextNode(msg));
         messages.appendChild(li);
     });
+    socket.on('refresh', function(){
+        refreshUsers();
+    })
 }]);
